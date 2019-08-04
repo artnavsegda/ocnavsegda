d8704ae4feab1027dc68c6637c59f17a98ecf159
Art Navsegda <artnavsegda@gmail.com>
2019-08-04 16:11:54 +0300

    Create calendar.m

---
+++ calendar.m
@@ -0,0 +1,17 @@
+#import <Foundation/Foundation.h>
+
+int main()
+{
+	NSDate *now = [NSDate date];
+	NSLog(@"%p", now);
+	NSLog(@"%@", now);
+
+	double seconds = [now timeIntervalSince1970];
+	NSLog(@"%f", seconds);
+
+	NSDate *later = [now dateByAddingTimeInterval:100000];
+	NSLog(@"%@", later);
+
+	NSCalendar *cal = [NSCalendar currentCalendar];
+	NSLog(@"%@", [cal calendarIdentifier]);
+}
