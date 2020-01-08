49540943df1c758be5b2af642c51d6fb9f985684
Art Navsegda <artnavsegda@gmail.com>
2020-01-08 07:33:09 +0300

    tak

---
+++ xcode/ocapp/ocapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ocapp
+//
+//  Created by Art Navsegda on 1/8/20.
+//  Copyright © 2020 Art Navsegda. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ xcode/ocapp/ocapp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ocapp
+//
+//  Created by Art Navsegda on 1/8/20.
+//  Copyright © 2020 Art Navsegda. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
