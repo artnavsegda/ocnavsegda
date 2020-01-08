2be0c97fd35e20e894c57c724083849e3eabb652
Art Navsegda <artnavsegda@gmail.com>
2020-01-08 07:28:12 +0300

    a

---
+++ xcode/ochello/ochello/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ochello
+//
+//  Created by Art Navsegda on 1/8/20.
+//  Copyright © 2020 Art Navsegda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
