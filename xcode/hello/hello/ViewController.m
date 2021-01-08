5ba5b1624f8a6993a81b9b7b46a73286537c7b91
Art Navsegda <artnavsegda@gmail.com>
2021-01-08 09:22:57 +0300

    Update ViewController.m

--- xcode/hello/hello/ViewController.m
+++ xcode/hello/hello/ViewController.m
@@ -18,7 +18,7 @@
     // Do any additional setup after loading the view.
     self.locationManager = [[CLLocationManager alloc] init];
     self.locationManager.delegate = self;
-    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"A77A1B68-49A7-4DBF-914C-760D07FBB87B"];
+    NSUUID *uuid = [[NSUUID alloc] initWithUUIDString:@"c7c1a1bf-bb00-4cad-8704-9f2d2917ded2"];
     self.myBeaconRegion = [[CLBeaconRegion alloc] initWithProximityUUID:uuid identifier:@"art.navsegda.testregion"];
     [self.locationManager startMonitoringForRegion:self.myBeaconRegion];
 }
