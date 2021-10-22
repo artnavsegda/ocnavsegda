2929c7375d0018cec900170a902d7824fbe1ae7b
Art Navsegda <artnavsegda@gmail.com>
2021-10-22 10:56:01 +0300

    deeplink

---
+++ deeplink/deeplink/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  deeplink
+//
+//  Created by Art Navsegda on 22.10.2021.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
+
